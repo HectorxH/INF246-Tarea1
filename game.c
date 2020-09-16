6b527f1827c840c43e8fed455c977efee5782903
Anastasiia Fedorova <miffyrli@gmail.com>
2020-09-16 14:48:00 -0300

    comentarios agregados

--- game.c
+++ game.c
@@ -1,6 +1,16 @@
 #include "game.h"
 
-
+/*
+ * Function: newGame
+ * ----------------------------
+ *   Crea un struct de tipo juego y inicializa sus campos.
+ *
+ *   char* name: string con nombre de juego.
+ *   char* path: string con direccion completa de juego.
+ *   int n: la cantidad de categorias del juego.
+ *
+ *   Returns: Game*, retorna el juego creado.
+ */
 Game* newGame(char* name, char* path, int n_categories){
     Game* g = (Game*)malloc(sizeof(Game));
 
@@ -11,14 +21,42 @@ Game* newGame(char* name, char* path, int n_categories){
     return g;
 }
 
+/*
+ * Function: compareGame
+ * ----------------------------
+ *   Función que permite comparar las cantidades de categorias de dos juegos.
+ *
+ *   const void* g1: primer juego
+ *   const void* g2: segundo juego
+ *
+ *   Returns: int, diferencia entre las cantidades decategorias de dos juegos.
+ */
 int compareGame(const void* g1, const void* g2){
     return (*(Game**)g1)->n_categories - (*(Game**)g2)->n_categories;
 }
 
+/*
+ * Function: freeGame
+ * ----------------------------
+ *   Libera la memoria asignada al struct de juego, pasado como parametro.
+ *
+ *   Game* g: el juego a liberar.
+ *
+ *   Returns: void, no retorna nada.
+ */
 void freeGame(Game* g){
     free((void*)g);
 }
 
+/*
+ * Function: printGame
+ * ----------------------------
+ *   Muestra por pantalla los datos de campos de struct, según el formato dado.
+ *
+ *   Game* g: el juego cuyos datos se desea mostrar por pantalla.
+ *
+ *   Returns: void, no retorna nada.
+ */
 void printGame(Game* g){
     FILE* fp = fopen(g->path, "r");
     if(fp == NULL){
@@ -38,7 +76,7 @@ void printGame(Game* g){
     printf("Desarrollador:\n\t%s", buffer);
 
     fgets(buffer, MAX_PATH-1, fp);
-    printf("Descripcion:\n\t%s", buffer);
+    printf("Descripción:\n\t%s", buffer);
     printf("----------------------------------------\n\n");
 
     fclose(fp);
